529642f7a71a7b26553d5ad4271e9c001b9979c9
rin-okumura1 <davidnobati@gmail.com>
2022-09-29 22:24:10 -0300

    migration seed

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var repositories=require("../src/repositories/repusers")
-const usersList=[ {
-    
-  "dni":123456,
-  "name": "caro ",
-  "surname":"pacheco",
-  "email_address" : "lalala@correo.com",
-  "password":123554,
-  "qualification":5,
-  "status2":"active"
-},{
-    
-  "dni":123456,
-  "name": "david",
-  "surname":"pacheco",
-  "email_address" : "lalala@correo.com",
-  "password":123554,
-  "qualification":5,
-  "status2":"active"
-},{
-    
-  "dni":123456,
-  "name": "ago",
-  "surname":"pacheco",
-  "email_address" : "lalala@correo.com",
-  "password":123554,
-  "qualification":5,
-  "status2":"active"
-},
-{
-    
-  "dni":123456,
-  "name": "nelson",
-  "surname":"pacheco",
-  "email_address" : "lalala@correo.com",
-  "password":123554,
-  "qualification":5,
-  "status2":"active"
-}]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json([{
