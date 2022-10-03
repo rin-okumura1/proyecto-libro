ab3b7445d4f70011952f1a854936b9f1400bb42a
rin-okumura1 <davidnobati@gmail.com>
2022-10-02 21:38:42 -0300

    oka

--- src/repositories/users.js
+++ src/repositories/users.js
@@ -1,12 +1,15 @@
+const {Users}=require("../../db/models")
+
+
 async function getAll(){
   return await Users.findAll()
   }
   
 async function getById(id){
-    return await users.findById(id)
+    return await Users.findById(id)
     }
-    const {Users}=require("D:\ort\2 anio\tp2\proyecto libros\db\models")
-const users = require("../../db/models/users")
+    
+
     
     module.exports={
         getById,
