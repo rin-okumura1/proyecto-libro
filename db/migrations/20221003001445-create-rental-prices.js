b10cfb867e945662beb05ffcf5c083921824625d
nmanoche <nelson.manoche@gmail.com>
2022-10-02 21:27:12 -0300

    migration of model rental prices

---
+++ db/migrations/20221003001445-create-rental-prices.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RentalPrices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      bookId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RentalPrices');
+  }
+};
---
+++ db/seeders/20221003001741-sample-rental-prices.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("RentalPrices",
+      [
+        {
+          price: 1500,
+          bookId: 1,
+          'createdAt': "2022-01-01 22:58:01",
+          'updatedAt': "2022-01-01 22:58:01"
+        },
+        {
+          price: 4369.45,
+          bookId: 3,
+          'createdAt': "2022-01-01 22:58:01",
+          'updatedAt': "2022-01-01 22:58:01"
+        },
+      ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("RentalPrices", null, {});
+  },
+};
