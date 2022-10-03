1ecd2e5155b923ca07c3ca42d2aa4e27e410b19c
Caro Paz <pazcaro979@gmail.com>
2022-10-03 12:05:38 -0300

    miration y models

---
+++ db/models/exchange.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Exchange extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Exchange.init({
+    bookId1: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    bookId2: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull:false
+  }
+  }, {
+    sequelize,
+    modelName: 'Exchange',
+    tableName: 'Exchanges'
+  });
+  return Exchange;
+};
