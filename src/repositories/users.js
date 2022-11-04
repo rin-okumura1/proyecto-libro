4c7e65e302bd0fb952a3e75587ef077a94af4b53
rin-okumura1 <davidnobati@gmail.com>
2022-11-03 22:02:28 -0300

    update

--- src/repositories/users.js
+++ src/repositories/users.js
@@ -1,54 +1,12 @@
-const {Users,Status} = require("../../db/models")
 
-const {
-  param
-} = require("../../routes")
-
-
-async function exists(email) {
-return await Users.findOne({
-where:{email}
-})
-
-}
-
-async function getAll(params = {}) {
-  let query = {
-    where: {},
-    include: [Status]
+const {Users,Status}=require("../../db/models")
+async function getAll(){
+  return await Users.findAll({include:[Status]})
   }
-
-  if (params.statusId) {
-    query.where = {
-      statusId: params.statusId
+async function getById(id){
+  return await Users.findByPk(id)
     }
-  }
-
-
-
-  return await Users.findAll(query)
-}
-
-async function getById(id) {
-  return await Users.findByPk(id, {
-    include: [Status]
-  })
-}
-
-async function saveUser(data) {
-  return await Users.create({
-    "name": data.name,
-    "surname": data.surname,
-    "email": data.email,
-    "password": data.password,
-    "score": data.score,
-    "statusId": 2
-  })
-}
-
-module.exports = {
-  getById,
-  getAll,
-  saveUser,
-  exists
-}
+    module.exports={
+        getById,
+        getAll
+    }
