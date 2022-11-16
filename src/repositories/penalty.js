592f4d3a87ffc542e26058e7bdde5d543b70137f
Caro Paz <pazcaro979@gmail.com>
2022-11-16 20:48:24 -0300

    first-update test-penalty

--- src/repositories/penalty.js
+++ src/repositories/penalty.js
@@ -28,40 +28,34 @@ const getById = async (id) => {
 };
 
 async function generarPenalidad(userId){
-    // usuario  no tiene registro de sanciones --> se crea nuevo registro [X]
-    // existiendo.. si tiene cantidad penalidades entonces:
-    //  1.si tiene fecha vencida --> update (id, cant, fecha)
-    //  2.si tiene fecha vigente  --> sumarFechaSancionVigente(id, cant,fecha)
-   let penalty = await getPenaltyByIdObj(userId)
+    
+   let penalty = await getPenaltyByIdUser(userId)
+   if (!penalty){  // si no existe registro de sanciones
+    penalty=await createPenalty(userId)
+  
+   } 
    let dateNow = await date.getDateNow()
    let dateToPenalty = await date.setFormatDateToExpect(penalty.dateTo)
     
-    if (!penalty){  // si no existe registro de sanciones
-        penalty=createPenalty(userId)
-    } 
     if (penalty.cantPenalty<10){  // si la penalidad es menor a 10
-        if (dateToPenalty <= dateNow){ // fecha vencida 
+       
+    if (dateToPenalty <= dateNow){ // fecha vencida 
             updatedPenalty(penalty.id, penalty.cantPenalty)
-       } else {  // fecha vigente
+       } 
+    else {  // fecha vigente
         dateToPenalty= date.updateDateForPenalty (penalty.dateTo)  // actualiza el dateTo
         updatedPenalty(penalty.id, penalty.cantPenalty, dateToPenalty )
        }
-    } else {    // si la penalidad es mayor a 10 se cambia el status del usuario
-        console.log("si es mayor a 10 cantpenalty");
-        let userObj = Users.getObjUser(userId)
-        console.log(userObjt.id);
-        //buscar el status con statusID que tenemos con el usuario
-        // modificamos el status de disponible a no disponible.
+    } 
+    else {    // si la penalidad es mayor a 10 se cambia el status del usuario
+      let user = await userRep.getById(userId)
+      userRep.changeStatus(user.id,DISABLE)
     }
-    
-    
-
-
     
     
 }
 
-function getPenaltyByIdObj(userId){
+function getPenaltyByIdUser(userId){
     return  Penalty.findOne({
       where: {
         userId: userId
@@ -107,6 +101,6 @@ module.exports = {
     getById, 
     getAll,
     generarPenalidad,
-    getPenaltyByIdObj,
+    getPenaltyByIdUser,
     updatedPenalty,
 }
---
+++ test/penalty.js
@@ -0,0 +1,133 @@
+const { assert } = require('chai')
+const request = require('supertest')
+const app = require('../app')
+var penalties = require('../src/repositories/penalty')
+var user = require('../src/repositories/users')
+
+
+
+
+
+describe('Penalty', function() {
+    describe('Registrar una penalidad', function() {
+
+        before(function () {
+            
+          });
+        it('Se requiere crear un registro de penalizacion a usuario', async function() {
+            // crear un registro en el cual el usuario no este registrado en la tabla de penalidades
+            let userId=4
+            penalty= await penalties.getPenaltyByIdUser(userId)
+            if (!penalty){
+                 await penalties.generarPenalidad(userId)
+            }
+            penalty = await penalties.getPenaltyByIdUser(userId) 
+            if (penalty){
+                result=true
+            }
+            assert.equal(result,true)
+        })
+
+        
+        //------------------------------------------------------------------
+
+       /* it('Se requiere aumentar la cantidad de penalidades con fecha dateTo no vigente', async function() {
+            // tener un registro en el cual el usuario este en la tabla de penalidades con fecha dateto vencida
+            let userId=5
+            let beforeCantPenalty
+            let afterCantPenalty
+
+            let penalty= await penalties.getPenaltyByIdUser(userId)
+            
+            beforeCantPenalty = penalty.cantPenalty
+           
+
+            if (!penalty){
+                 await penalties.generarPenalidad(userId)
+            }
+            
+            penalty = await penalties.getPenaltyByIdUser(userId) 
+            afterCantPenalty = penalty.cantPenalty
+
+            console.log(beforeCantPenalty);
+            console.log(afterCantPenalty);
+            
+            if (beforeCantPenalty<afterCantPenalty){
+                result=true
+            }
+            assert.equal(result,true)
+        })
+
+
+     /*   //--------------------------------------------------------------------------
+        it('Se requiere aumentar la cantidad de penalidades y actualizar dateTo vigente', async function() {
+            // tener un registro en el cual el usuario este en la tabla de penalidades con fecha dateto vigente
+            let userId=5
+            let beforeCantPenalty
+            let afterCantPenalty
+
+            let penalty= await penalties.getPenaltyByIdUser(userId)
+            beforeCantPenalty = penalty.cantPenalty
+            console.log(penalty.cantPenalty); 
+
+            if (!penalty){
+                 await penalties.generarPenalidad(userId)
+            }
+            
+            penalty = await penalties.getPenaltyByIdUser(userId) 
+            console.log(penalty.cantPenalty); 
+            afterCantPenalty = penalty.cantPenalty
+
+            if (beforeCantPenalty>afterCantPenalty){
+                result=true
+            }
+            assert.equal(result,true)
+        })
+
+
+        //--------------------------------------------------------------------------------
+
+        it('Se requiere cambiar el estado del usuario cuando la cantidad de penalidad supere el maximo de 10', async function() {
+            // tener un registro en el cual el usuario tenga 10 penalidades
+            let userId=4
+            penalty= await penalties.getPenaltyByIdUser(userId)
+            if (!penalty){
+                 await penalties.generarPenalidad(userId)
+            }
+            penalty = await penalties.getPenaltyByIdUser(userId) 
+            if (penalty){
+                result=true
+            }
+            assert.equal(result,true)
+        })
+        */
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
