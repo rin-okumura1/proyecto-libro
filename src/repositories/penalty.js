6231a3b460ebdb180091feb94e500f1b61c983bf
Caro Paz <pazcaro979@gmail.com>
2022-11-17 23:27:31 -0300

    update new mejoradisima - test penalty

--- src/repositories/penalty.js
+++ src/repositories/penalty.js
@@ -1,25 +1,29 @@
-const {Penalty, Users} = require ('../../db/models')
+const {
+  Penalty,
+  Users
+} = require('../../db/models')
 var userRep = require('./users')
 
-var date= require('./date')
-const DISABLE =1;
+var date = require('./date')
+const DISABLE = 1;
 const MAX_PENALTY = 10;
 
 
 const getAll = async (params = {}) => {
-    let query = {
-        where: {},
-        attributes: { exclude: ['id'] },
-        include: [
-            { 
-                model: Users ,
-                attributes: ['id', 'name', 'surname', 'email']
-            }]
-}
-return await Penalty.findAll(query);
+  let query = {
+    where: {},
+    attributes: {
+      exclude: ['id']
+    },
+    include: [{
+      model: Users,
+      attributes: ['id', 'name', 'surname', 'email']
+    }]
+  }
+  return await Penalty.findAll(query);
 };
 async function getPenaltyByIdUser(uID) {
-  return  Penalty.findOne({
+  return await Penalty.findOne({
     where: {
       userId: uID
     }
@@ -27,90 +31,128 @@ async function getPenaltyByIdUser(uID) {
 }
 
 const getById = async (paramsId) => {
-  return  Penalty.findOne({
+  return await Penalty.findOne({
     where: {
       id: paramsId
     }
   })
 }
 
-async function generarPenalidad(userId){
-    // usuario  no tiene registro de sanciones --> se crea nuevo registro [X]
-    // existiendo.. si tiene cantidad penalidades entonces:
-    //  1.si tiene fecha vencida --> update (id, cant, fecha)
-    //  2.si tiene fecha vigente  --> sumarFechaSancionVigente(id, cant,fecha)
-   let penalty = await getPenaltyByIdUser(userId)
-   let dateNow = await date.getDateNow()
+/*async function generarPenalidad(userId) {
+  // usuario  no tiene registro de sanciones --> se crea nuevo registro [X]
+  // existiendo.. si tiene cantidad penalidades entonces:
+  //  1.si tiene fecha vencida --> update (id, cant, fecha)
+  //  2.si tiene fecha vigente  --> sumarFechaSancionVigente(id, cant,fecha)
+  console.log(userId);
+
+  let penalty = await getPenaltyByIdUser(userId)
+  let dateNow = await date.getDateNow()
+  console.log(penalty);
 
  
-   console.log(penalty);
-
-   
+  if (!penalty) { // si no existe registro de sanciones
     
-    if (!penalty){  // si no existe registro de sanciones
-      console.log("que no existe registro");
-        await createPenalty(userId)
-    } 
+    return await createPenalty(userId)
+  }
 
-    if (penalty){
-      
-       
+  if (penalty) {
+    
+
     let dateToPenalty = date.setFormatDateToExpect(penalty.dateTo)
-    if (penalty.cantPenalty<MAX_PENALTY){  // si la penalidad es menor a 10
-        if (dateToPenalty <= dateNow){ // fecha vencida 
-            updatedPenalty(penalty.id, penalty.cantPenalty)
-       } 
-    else {  // fecha vigente
-        dateToPenalty= date.updateDateForPenalty (penalty.dateTo)  // actualiza el dateTo
-        updatedPenalty(penalty.id, penalty.cantPenalty, dateToPenalty )
-       }
-    } else {    // si la penalidad es mayor a 10 se cambia el status del usuario
-        let user = await userRep.getById(userId)
-        userRep.changeStatus(user.id,DISABLE)
+    if (penalty.cantPenalty < MAX_PENALTY) { // si la penalidad es menor a 10
+      
+      if (dateToPenalty <= dateNow) { // fecha vencida 
+        
+        await updatedPenalty(penalty.id, penalty.cantPenalty)
+      } else { // fecha vigente
+        console.log("en la fecha vigente");
+        dateToPenalty = date.updateDateForPenalty(penalty.dateTo) // actualiza el dateTo
+        await updatedPenalty(penalty.id, penalty.cantPenalty, dateToPenalty)
+      }
+    } else { // si la penalidad es mayor a 10 se cambia el status del usuario
+      let user = await userRep.getById(userId)
+      userRep.changeStatus(user.id, DISABLE)
     }
   }
-    
+
+}*/
+
+
+
+function getDatePenalty(penalty) {
+  return date.setFormatDateToExpect(penalty.dateTo)
 }
 
+async function generarPenalidad(userId) {
+  let penalty = await getPenaltyByIdUser(userId)
+  if (!penalty) {
+      return await createPenalty(userId)
+  }
+  if (penalty.cantPenalty < MAX_PENALTY) {
+      return await penaltyForExpired(penalty)
+  }
+  return await disableUser(userId)
+}
 
-
-async function createPenalty(userId){
+async function updatePenaltyDate(penalty) {
   
-let dateTo = date.getDateForPenalty()
-    return await Penalty.create({
-        userId,
-        dateTo,
-      })
+  const dateToPenalty = date.updateDateForPenalty(penalty.dateTo) // actualiza el dateTo
+  
+  return await updatedPenalty(penalty.id, penalty.cantPenalty, dateToPenalty)
+}
+
+async function penaltyForExpired(penalty) {
+  let dateNow = await date.getDateNow()
+  let dateToPenalty = getDatePenalty(penalty)
+  if (dateToPenalty <= dateNow) { // fecha vencida 
+      return await updatedPenalty(penalty.id, penalty.cantPenalty)
+  }
+  return updatePenaltyDate(penalty)
+}
+
+async function disableUser(user) {
+  await userRep.getById(userId)
+  userRep.changeStatus(user.id, DISABLE)
 
 }
-async function updatedPenalty (penaltyId, cantPenalty, dateTo){
-    
+async function createPenalty(userId) {
 
-    if (!dateTo) {
-        dateTo= date.getDateForPenalty()
-    }
-    cantPenalty ++
-    
-    return await Penalty.update(
-      {
-        cantPenalty: cantPenalty,
-        dateTo: dateTo
-      },
-      {
-        where: {
-          id: penaltyId
-        }
+  let dateTo = date.getDateForPenalty()
+  return await Penalty.create({
+    userId,
+    dateTo,
+  })
+
+}
+async function updatedPenalty(penaltyId, cantPenalty, dateTo) {
+  
+  if (!dateTo) {
+    dateTo = date.getDateForPenalty()
+  }
+  cantPenalty++
+
+  return await Penalty.update(
+
+    {
+
+      cantPenalty: cantPenalty,
+      dateTo: dateTo
+    }, {
+      where: {
+        id: penaltyId
       }
-)};
+    }
+  )
+};
 
 
 
 
 
 module.exports = {
-    getById, 
-    getAll,
-    generarPenalidad,
-    updatedPenalty,
-    getPenaltyByIdUser,
+  getById,
+  getAll,
+  generarPenalidad,
+  updatedPenalty,
+  getPenaltyByIdUser,
 }
--- test/penalty.js
+++ test/penalty.js
@@ -1,33 +1,56 @@
-const { assert, expect } = require('chai')
+const {
+    assert,
+    expect
+} = require('chai')
 const request = require('supertest')
 const app = require('../app')
 var penalties = require('../src/repositories/penalty')
 var ren = require('../routes/rental')
 var date = require('../src/repositories/date')
-const {book, Rental, Users, Penalty } = require("../db/models")
+const {
+    book,
+    Rental,
+    Users,
+    Penalty
+} = require("../db/models")
 let uIdNow
 
 
 
 
-async function createRental (data) {
+async function createRental(data) {
 
-    const { userId, bookId, dateFrom, dateToExpect, dateToReal } = data;
+    const {
+        userId,
+        bookId,
+        dateFrom,
+        dateToExpect,
+        dateToReal
+    } = data;
 
-        const newdRental =await  Rental.create({
-            "userId":userId, 
-            "bookId": bookId, 
-            "dateFrom": dateFrom,
-            "dateToExpect": dateToExpect,
-            "dateToReal":dateToReal 
-           });
-    
-           return newdRental
-        }
+    const newdRental = await Rental.create({
+        "userId": userId,
+        "bookId": bookId,
+        "dateFrom": dateFrom,
+        "dateToExpect": dateToExpect,
+        "dateToReal": dateToReal
+    });
+
+    return newdRental
+}
 
 async function createBook(data) {
-const { authorId, editionYear, title, categoryId, languageId, synopsis, availabilityId, userId } = data;
-    const newdBook =await  book.create({
+    const {
+        authorId,
+        editionYear,
+        title,
+        categoryId,
+        languageId,
+        synopsis,
+        availabilityId,
+        userId
+    } = data;
+    const newdBook = await book.create({
         "authorId": authorId,
         "editionYear": editionYear,
         "title": title,
@@ -36,172 +59,178 @@ const { authorId, editionYear, title, categoryId, languageId, synopsis, availabi
         "synopsis": synopsis,
         "availabilityId": availabilityId,
         "userId": userId
-       });
+    });
 
-       return newdBook
-    }
+    return newdBook
+}
 
 async function createUser(data) {
-        const { name, surname, email, password } = data;
-            const newdUser =await  Users.create({
-                "name": name,
-                "surname": surname,
-                "email": email,
-                "password": password,
-               });
-        
-               return newdUser
-            }
+    const {
+        name,
+        surname,
+        email,
+        password
+    } = data;
+    const newdUser = await Users.create({
+        "name": name,
+        "surname": surname,
+        "email": email,
+        "password": password,
+    });
+
+    return newdUser
+}
 async function createPenalty(data) {
 
-        const { userId, cantPenalty, dateTo } = data;
-            const newdPenalty =await  Penalty.create({
-                "userId": userId,
-                "cantPenalty": cantPenalty,
-                "dateTo": dateTo
-               });
-        
-               return newdPenalty
-            }
+    const {
+        userId,
+        cantPenalty,
+        dateTo
+    } = data;
+    const newdPenalty = await Penalty.create({
+        "userId": userId,
+        "cantPenalty": cantPenalty,
+        "dateTo": dateTo
+    });
+
+    return newdPenalty
+}
 async function deleteUser(i) {
-    let user = await Users.findOne({
+    return await Users.destroy({
         where: {
-            id:i
+            id: i
         }
-      })
-    await user.destroy();
-    return user;
+    })
+   
+}
+
+async function deletePenalty(uId) {
+     return await Penalty.destroy({
+        where: {
+            "userId": uId
         }
-
- async function deletePenalty(uId) {
-            let penalty = await Penalty.findOne({
-                where: {
-                    "userId": uId
-                }
-              })
-            await penalty.destroy();
-            return penalty ;
-                }
- async function deleteBook(i) {
-                    let bookDelete = await book.findOne({
-                        where: {
-                            "id": i
-                        }
-                      })
-                    await bookDelete.destroy();
-                    return bookDelete ;
-                        }
-async function deleteRental(id) {
-                            let rental= await Rental.findOne({
-                                where: {
-                                    "id": id
-                                }
-                              })
-                            await rental.destroy();
-                            return rental ;
-                                }
-
-
-                
-
-describe('Penalty', function() {
-    let u
-    let b
-    let r
+    });
     
-
-    before(async function ()  {
-       
-       let dataUser = {
-             "name": "Juan",
-             "surname": "Perez",
-             "email": "juanperez@correo.com",
-             "password": "123456"
+    
+}
+async function deleteBook(i) {
+    return await book.destroy({
+        where: {
+            "id": i
         }
-       u =await   createUser(dataUser)
-        
-
-       let dataBook ={
-        
-        "authorId": 1,
-        "editionYear": 2018,
-        "title": "Test",
-        "categoryId": 1,
-        "languageId": 1,
-        "synopsis":
-          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. masa aeneana. Cum sociis natoque penatibus et",
-        "availabilityId": 1,
-        "userId": u.id
-      
-      } 
-       b= await createBook(dataBook)
-       
+    })
+    
+}
+async function deleteRental(id) {
+    return await Rental.destroy({
+        where: {
+            "id": id
+        }
+    })
+   
+}
 
 
-        let dataRental= {
-            "userId": u.id, 
-            "bookId": b.id, 
+
+
+describe('Penalty', function () {
+    let userDescribe
+    let bookDescribe
+    let rentalDescribe
+
+
+    before(async function () {
+
+        let dataUser = {
+            "name": "Juan",
+            "surname": "Perez",
+            "email": "juanperez@correo.com",
+            "password": "123456"
+        }
+        userDescribe = await createUser(dataUser)
+
+
+        let dataBook = {
+
+            "authorId": 1,
+            "editionYear": 2018,
+            "title": "Test",
+            "categoryId": 1,
+            "languageId": 1,
+            "synopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. masa aeneana. Cum sociis natoque penatibus et",
+            "availabilityId": 1,
+            "userId": userDescribe.id
+
+        }
+        bookDescribe = await createBook(dataBook)
+
+
+
+        let dataRental = {
+            "userId": userDescribe.id,
+            "bookId": bookDescribe.id,
             "dateFrom": "2022-11-09",
             "dateToExpect": "2022-11-10",
-            "dateToReal":"2022-11-10" 
+            "dateToReal": "2022-11-10"
         }
 
-        r= await createRental(dataRental)
-        
-        
+        rentalDescribe = await createRental(dataRental)
 
-        
+
+
+
     })
-    describe ('Registrar primera penalidad',async function()
-    {
-        it('Se requiere crear un registro de penalizacion a usuario',async function(done) {
-        // console.log("dentro del primer it");
-        //console.log(u);
-        //console.log(b);
-        //console.log(r);
-           let param = r.id;
-
-         //  p  = await penalties.getPenaltyByIdUser(u.id)
-                //console.log(p);
-        
-               /*  request(app)
-                .put('/rental/:param')
-                .send({
-                    "dateToReal":"2022-11-17"
-                })
-                .expect(201,done())*/
-                
+    describe('Registrar primera penalidad', async function () {
+        skip.it('Se requiere crear por primera vez  un registro de penalizacion a usuario ', async function () {
             
-        
+            let penalty = await penalties.getPenaltyByIdUser(userDescribe.id)
+            if (penalty == null) {
+                await penalties.generarPenalidad(userDescribe.id)
+            }
+            penalty = await penalties.getPenaltyByIdUser(userDescribe.id)
+            assert.isNotNull(penalty, 'se invoco mismo metodo para penalty, paso de null a !null');
 
         })
 
-        
+
 
 
     }) // termina primer describe de registrar primera penalidad
-    after(async function ()  {
-        //await deleteUser(u.id)
-        //await deletePenalty(u.id)
-        //await deleteBook(b.id)
-        //await deleteRental(r.id)
+    after(async function () {
+        await deletePenalty(userDescribe.id)
+    });
+
+    describe('Actualizaciones de Penalty', async function () {
+        skip.it('Se requiere aumentar la cantidad de penalidad y actualizar dateTo no vigente', async function () {
+            let dataPenalty = {
+                "userId": userDescribe.id,
+                "cantPenalty": 1,
+                "dateTo": "2022-11-16"
+            }
+            let pBefore = await createPenalty(dataPenalty)
+            let cantPenaltyBefore = pBefore.cantPenalty
+            if (cantPenaltyBefore == 1) { 
+                await penalties.generarPenalidad(userDescribe.id)
+            }
+            penaltyFinal = await penalties.getPenaltyByIdUser(userDescribe.id)
+            
+            cantPenaltyAfter = penaltyFinal.cantPenalty
+
+            assert.equal(2, cantPenaltyAfter, 'se actualiza el cantPenalty de 1 a 2');
+
+        })
+        after(async function () {
+            await deletePenalty(userDescribe.id)
         });
-    
+
+    }) // termina segundo describe de registrar primera penalidad
+
+
+    after(async function () {
+        await deleteUser(userDescribe.id)
+        await deletePenalty(userDescribe.id)
+        await deleteBook(bookDescribe.id)
+        await deleteRental(rentalDescribe.id)
+    });
+
 }); // termina el describe de penalty general
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
